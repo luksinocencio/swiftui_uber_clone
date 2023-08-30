68c5b331f7cf90639882499bdae4d7116fb11a46
Lucas Inocêncio <luksinocencio@gmail.com>
2023-08-30 04:02:24 -0300

    Feat/searching for locations (#1)
    * construção da tela de procurar localização
    
    * searching for a location
    
    * selecting a collection

--- UberCloneSwiftUI/UberCloneSwiftUI/App/UberCloneSwiftUIApp.swift
+++ UberCloneSwiftUI/UberCloneSwiftUI/App/UberCloneSwiftUIApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct UberCloneSwiftUIApp: App {
+    @StateObject var locationViewModel = LocationSearchViewModel()
+    
     var body: some Scene {
         WindowGroup {
             HomeView()
+                .environmentObject(locationViewModel)
         }
     }
 }
---
+++ UberCloneSwiftUI/UberCloneSwiftUI/Core/LocationSearch/ViewModel/LocationSearchViewModel.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+import MapKit
+
+class LocationSearchViewModel: NSObject, ObservableObject {
+    // MARK: - Properties
+    
+    @Published var results = [MKLocalSearchCompletion]()
+    @Published var selectedLocation: String?
+    
+    private let searchCompleter = MKLocalSearchCompleter()
+    var queryFragment: String = "" {
+        didSet {
+//            print("DEBUG: Query fragment is \(queryFragment)")
+            searchCompleter.queryFragment = queryFragment
+        }
+    }
+    
+    override init() {
+        super.init()
+        searchCompleter.delegate = self
+        searchCompleter.queryFragment = queryFragment
+    }
+    
+    // MARK: - Helpers
+    func selectedLocation(_ location: String) {
+        self.selectedLocation = location
+        print("DEBUG: selected location is \(self.selectedLocation)")
+    }
+}
+
+// MARK: - MKLocalSearchCompleterDelegate
+
+extension LocationSearchViewModel: MKLocalSearchCompleterDelegate {
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        self.results = completer.results
+    }
+}
